bb9b5082fb9029dcabdc07177d59927265881132
Nguyen Tu Duc <duc7.nguyen@gmail.com>
2014-07-31 00:47:26 +0700

    pass test cases mentioned in file assignment.

--- best.R
+++ best.R
@@ -1,6 +1,29 @@
 best <- function(state, outcome) {
     ## Read outcome data
+    data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+    for(i in c(11, 17, 23)) {
+        data[, i] <- suppressWarnings(as.numeric(data[, i]))
+    }
+    
     ## Check that state and outcome are valid
+    if (!is.element(state, unique(data$State))) {
+        stop("invalid state")
+    }
+    if (!is.element(outcome, c("heart attack", "heart failure", "pneumonia"))) { 
+        stop("invalid outcome")
+    }
+    
     ## Return hospital name in that state with lowest 30-day death
     ## rate
+    col <- switch(outcome,
+                  "heart attack"=11, "heart failure"=17, "pneumonia"=23)
+    
+    statedata <- subset(data, data[,7]==state)
+    #     statedata[, col] <- as.numeric(statedata[, col])
+    statedata <- na.omit(statedata)
+    min_val <- min(statedata[,col])
+    hospitals <- statedata[statedata[,col]==min_val,][,2]
+
+    hospitals
+    
 }
--- test_best.R
+++ test_best.R
@@ -1,4 +1,27 @@
 source("best.R")
 
+try2 <- function(code, silent = FALSE) {
+    tryCatch(code, error = function(c) {
+        msg <- conditionMessage(c)
+        if (!silent) message(c)
+        invisible(structure(msg, class = "try-error"))
+    })
+}
+
+print("This is test cases for function best + expected output")
 best("TX", "heart attack")
 # "CYPRESS FAIRBANKS MEDICAL CENTER"
+
+best("TX", "heart failure")
+#[1] "FORT DUNCAN MEDICAL CENTER"
+
+best("MD", "heart attack")
+#[1] "JOHNS HOPKINS HOSPITAL, THE"
+best("MD", "pneumonia")
+#[1] "GREATER BALTIMORE MEDICAL CENTER"
+best("BB", "heart attack")
+#Error in best("BB", "heart attack") : invalid state
+best("NY", "hert attack")
+#Error in best("NY", "hert attack") : invalid outcome
+
+
